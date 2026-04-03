f0c726507cef587f700a42e22ee516a30b7ac660
Nicolas Sitartchouk <nicolas.sitartchouk@gmail.com>
2026-04-03 12:33:47 -0400

    Compartmentalised the main dashboard; finalised 'add' button and replaced basic log with the beginnings of 'view log' button.
    Decoupled several items; created private classes for each dashboard button.
    
    Signed-off-by: Nicolas Sitartchouk <nicolas.sitartchouk@gmail.com>

--- Debugger/src/MainDashboard.java
+++ Debugger/src/MainDashboard.java
@@ -1,3 +1,4 @@
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.TextField;
@@ -8,19 +9,22 @@ import java.util.function.Predicate;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 public class MainDashboard extends JPanel
 {
     ArrayList<Bug> bugs = new ArrayList<>(); 
     public MainDashboard()
     {
-        bugs.add(new Bug("test1", new User("JOHN DOE")));
+        bugs.add(new Bug("Editing test 1 editing", new User("JOHN DOE")));
         bugs.add(new Bug("Strange bump on the road", new User("JANE DOE")));
         bugs.add(new Bug("Odd failure in system A", new User("Undefined")));
+        bugs.add(new Bug("Awesome reference!", new User("Sand sous l'Histoire")));
         this.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         JButton logButton = new JButton("View Log");
         
+        logButton.addActionListener(new BugLogButton());
 
 
 
@@ -28,75 +32,171 @@ public class MainDashboard extends JPanel
 
         JButton addButton = new JButton("Add Bug");
         
-        addButton.addActionListener(new ActionListener() {
+        addButton.addActionListener(new NewBugButton());
+        
+        
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        c.gridheight = 2;
+        this.add(logButton,c);
+        c.gridx = 2;
+        this.add(editButton,c);
+        c.gridx = 4;
+        this.add(addButton,c);
+
+
+    }
+
+    
+    private class BugLogButton implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            JFrame bugViewWindow = new JFrame("Bug Log");
+            JPanel holder = new JPanel(new GridBagLayout());
+            GridBagConstraints c = new GridBagConstraints();
+            JLabel searchLabel = new JLabel("Filter by keyword: ");
+            TextField searchField = new TextField(6);
+
+
+            c.gridx = 0;
+            c.gridy = 0;
+            c.gridwidth = 3;
+            holder.add(searchLabel,c);
+            c.gridx = 3;
+            c.gridwidth = 6;
+            holder.add(searchField,c);
+            c.gridx = 0;
+            c.gridy = 1;
+
+            JPanel bugHolder = new JPanel(new GridBagLayout());
+            GridBagConstraints g = new GridBagConstraints();
+
+            holder.setPreferredSize(new Dimension(500,500));
+            holder.add(bugHolder,c);
+
+            for(Bug b : bugs)
+            {
+                bugHolder.add(new BugRenderer(b),g);
+                g.gridy++;
+            }
+
+            searchField.addActionListener(new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    // TO FIX: potentially attempting to access larger substring than exists. Change sf.getText().length() to a case-based implementation: i.e. pick the shorter of the two
+                    Predicate<Bug> bugPredicate= b -> b.getDescription().substring(0,searchField.getText().length()).compareTo(searchField.getText()) != 0;
+                    ArrayList<Bug> filteredList = (ArrayList<Bug>) bugs.clone();
+                    filteredList.removeIf(bugPredicate);
+                    for(Bug b : filteredList)
+                    {
+                        bugHolder.add(new BugRenderer(b),g);
+                        g.gridy++;
+                    }
+
+
+                }   
+
+
+            });
+            bugViewWindow.add(holder);
+            bugViewWindow.setPreferredSize(new Dimension(500,500));
+            bugViewWindow.pack();
+            bugViewWindow.setVisible(true);
+        }
+    }
+
+    private class NewBugButton implements ActionListener
+    {
         @Override
         public void actionPerformed(ActionEvent e) {
             JFrame bugInsertWindow = new JFrame("Bug Insertion Menu");
             JPanel holder = new JPanel(new GridBagLayout());
             GridBagConstraints mc = new GridBagConstraints();
+            mc.gridwidth = 2;
             mc.gridx = 0;
             mc.gridy = 0;
             
+            JLabel name = new JLabel("Please describe the encountered Bug: ");
+            holder.add(name,mc);
+
+            mc.gridx = 2;
+            TextField namevalue = new TextField(6);
+            holder.add(namevalue,mc);
+
+            mc.gridwidth = 2;
+            mc. gridx = 0;
+            mc.gridy = 1;
+            JLabel area = new JLabel("Where was this bug encountered?");
+            holder.add(area,mc);
+            JButton selectArea = new JButton("Select Location");
+            selectArea.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    String locations[] = {"Product Backlog", "Implementation A", "Test Case 2"};
+                    String location = (String) JOptionPane.showInputDialog(
+                        null,
+                        "Please select encounter location: ",
+                        "Locations below",
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        locations,
+                        locations[0]
+
+                    );
+                    selectArea.setText(location);
+                }
+            });
+            mc.gridwidth = 2;
+            mc. gridx = 2;
+            holder.add(selectArea,mc);
+
+            mc.gridwidth = 4;
+            mc.gridy = 5;
+            mc.gridheight = 2;
             
-            //JLabel 
+            JButton enter = new JButton("ENTER");
+            enter.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    String[] options = {"YES", "NO"};
+                    String selected = (String) JOptionPane.showInputDialog(
+                        null,
+                        """
+                        Is this information correct?: 
+                        Description:""" + namevalue.getText() + "\n\n" +
+                        "Found in: " + selectArea.getText(),
+                        "Submission Form",
+                        JOptionPane.YES_NO_CANCEL_OPTION,
+                        null,
+                        options,
+                        options[1]
+                    );
+
+                    if(selected.equals("YES"))
+                        {
+                            bugs.add(new Bug(namevalue.getText(), new User("user")));
+                            bugInsertWindow.setVisible(false);
+                        }
+                    
+                }
+            });
+
+            holder.add(enter,mc);
             
-        }});
+            bugInsertWindow.add(holder);
+            bugInsertWindow.setPreferredSize(new Dimension(500,500));
+            bugInsertWindow.pack();
+            bugInsertWindow.setVisible(true);
         
-        
-        
-        
-        JLabel searchLabel = new JLabel("Filter by keyword: ");
-        TextField searchField = new TextField(6);
-        c.gridx = 0;
-        c.gridy = 0;
-        c.gridwidth = 2;
-        this.add(logButton,c);
-        c.gridx = 1;
-        c.gridx = 3;
-        this.add(editButton,c);
-        c.gridx = 5;
-        this.add(addButton,c);
-        c.gridx = 8;
-        this.add(searchLabel,c);
-        c.gridx = 12;
-        c.gridwidth = 6;
-        this.add(searchField,c);
-
-        searchField.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                // TO FIX: potentially attempting to access larger substring than exists. Change sf.getText().length() to a case-based implementation: i.e. pick the shorter of the two
-                Predicate<Bug> bugPredicate= b -> b.getDescription().substring(0,searchField.getText().length()).compareTo(searchField.getText()) != 0;
-                ArrayList<Bug> filteredList = (ArrayList<Bug>) bugs.clone();
-                filteredList.removeIf(bugPredicate);
-
-
-
-            }   
-
-
-        });
-        
-        
-        JPanel bugListHolder = new JPanel();
-        bugListHolder.setLayout(new GridBagLayout());
-        GridBagConstraints internal = new GridBagConstraints();
-        internal.gridx = 0;
-        internal.gridy = 0;
-        c.gridx = 0;
-        c.gridy = 2;
-        c.gridwidth = 9;
-        this.add(bugListHolder,c);
-        for(Bug b : bugs)
-            {
-                bugListHolder.add(new BugRenderer(b),internal);
-                internal.gridy++;
-            }
-
+        }
     }
-
 
     private class BugRenderer extends JPanel
     {
@@ -110,15 +210,15 @@ public class MainDashboard extends JPanel
             c.gridwidth = 2;
             this.add(bugID,c);
             JLabel status = new JLabel("Status: " + b.getStatus());
-            c.gridx = 3;
-            c.gridy = 0;
+            c.gridx = 0;
+            c.gridy = 1;
             c.gridwidth = 2;
             this.add(status,c);
             // potentially change to JTextField?
             JLabel description = new JLabel(b.getDescription());
-            c.gridx = 0;
-            c.gridy = 1;
-            c.gridwidth = 2;
+            c.gridx = 2;
+            c.gridy = 0;
+            c.gridwidth = 4;
             this.add(description,c);
 
         }
